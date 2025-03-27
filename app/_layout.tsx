08eaadae96904a0931b45f7f5dab6b5bd791770e
Sam Tovey <sam.tovey@red61.com>
2025-03-27 11:59:26 +0000

    remove boilerplate components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" />
-        <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
   );
